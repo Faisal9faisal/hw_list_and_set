1beb5519c98db23c206f2499af89aa0d2986a94d
faisal <faisal.Nahi.ruh.k@tuwaiq.edu.sa>
2021-10-18 22:53:24 +0300

    part2

--- src/main/kotlin/part2.kt
+++ src/main/kotlin/part2.kt
@@ -1,8 +1,12 @@
 fun main(args: Array<String>) {
 
-
+        val numberList = mutableListOf<Int>(1, 3, 7, 8,9)
+          println(numberList)
+        numberList[3]=12
+          println(numberList)
+        numberList.removeAt(3)
+          println(numberList)
 }
-
 
 
 
